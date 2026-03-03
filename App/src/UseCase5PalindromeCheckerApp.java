54d82c3111001abe3d9d04cdd21629050b44352d
Grihika <grihikajeloka@gmail.com>
2026-03-03 16:43:56 +0530

    UC5-ReverseString

---
+++ App/src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+
+        System.out.println("=== Palindrome Checker App (UC5 - Stack Based) ===");
+        System.out.print("Input: ");
+
+        String input = scanner.nextLine();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop characters and compare
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is Palindrome?: " + (isPalindrome ? "True" : "False"));
+
+        scanner.close();
+    }
+}
